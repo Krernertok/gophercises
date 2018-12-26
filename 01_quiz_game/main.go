67da9be8461fc4a1a3224476d4f40a98927a6f5a
Antti Sivula <antti.j.sivula@gmail.com>
2018-12-26 14:59:31 +0200

    Improve first version of quiz

--- 01_quiz_game/main.go
+++ 01_quiz_game/main.go
@@ -2,39 +2,57 @@ package main
 
 import (
 	"encoding/csv"
+	"flag"
 	"fmt"
 	"io"
 	"log"
 	"os"
+	"strings"
 )
 
-var filepath = "data/questions.csv"
+var filepath *string
+var defaultPath = "data/quiz.csv"
 
 func init() {
 	log.SetOutput(os.Stdout)
+
+	pathUsage := "path to the CSV file containing the questions and answers"
+	filepath = flag.String("file", defaultPath, pathUsage)
 }
 
 func main() {
 	var numQuestions, numCorrect int
-	questions := getQuestions(filepath)
+
+	flag.Parse()
+	questions := getQuestions(*filepath)
+
+	fmt.Println("Answer the following questions as quickly as possible.")
+	fmt.Println("Press ENTER to start.")
+
+	var start string
+	fmt.Scanln(&start)
 
 	for _, q := range questions {
 		if len(q) != 2 {
 			continue
 		}
-		numQuestions++
 
+		// print the question
 		fmt.Println(q[0])
 
 		var answer string
 		fmt.Scanln(&answer)
+		answer = strings.ToLower(strings.TrimSpace(answer))
 
+		// check against the correct answer
 		if answer == q[1] {
 			numCorrect++
 		}
+
+		numQuestions++
 	}
 
-	fmt.Println("You got", numCorrect, "out of", numQuestions, "correct!")
+	fmt.Println("You got", numCorrect, "out of", numQuestions, "questions correct!")
 }
 
 func getQuestions(path string) [][]string {
