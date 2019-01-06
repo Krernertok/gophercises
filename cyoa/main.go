a1af08c56d9b332e8ca45b53704a5d0be83e1c73
Antti Sivula <antti.j.sivula@gmail.com>
2019-01-06 14:14:13 +0200

    Add template and rendering the template

--- cyoa/main.go
+++ cyoa/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"github.com/krernertok/gophercises/cyoa/story"
+	"html/template"
 	"net/http"
 )
 
@@ -14,12 +15,19 @@ func main() {
 		panic(err)
 	}
 
+	template, err := template.New("arc.html").ParseFiles("templates/arc.html")
+
+	if err != nil {
+		panic(err)
+	}
+
 	fmt.Println("Starting the server on :8080")
-	http.ListenAndServe(":8080", storyHandler{story})
+	http.ListenAndServe(":8080", storyHandler{story, template})
 }
 
 type storyHandler struct {
-	story map[string]story.Arc
+	story    map[string]story.Arc
+	template *template.Template
 }
 
 func (h storyHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
@@ -43,13 +51,16 @@ func (h storyHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	fmt.Fprintln(w, arc)
+	err := h.template.Execute(w, arc)
+	if err != nil {
+		panic(err)
+	}
 }
 
 /*
 1. Parsing JSON into maps and structs DONE
 2. HTTP server / Routing DONE
 	- Name of the arc should be the path
-3. Templates
+3. Templates DONE
 4. Styling
 */
