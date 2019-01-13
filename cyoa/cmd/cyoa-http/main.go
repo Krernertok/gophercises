a3655be2af0add8b3cc9557da327a0808fb66086
Antti Sivula <antti.j.sivula@gmail.com>
2019-01-13 22:22:08 +0200

    Add minor changes to cyoa-http main.go and the template

--- cyoa/cmd/cyoa-http/main.go
+++ cyoa/cmd/cyoa-http/main.go
@@ -10,9 +10,14 @@ import (
 	"strings"
 )
 
+const (
+	defaultJSONPath = "/mnt/c/dev/goworkspace/src/github.com/krernertok/gophercises/cyoa/data/gopher.json"
+	templatePath    = "/mnt/c/dev/goworkspace/src/github.com/krernertok/gophercises/cyoa/templates/arc.html"
+	baseDir         = "/mnt/c/dev/goworkspace/src/github.com/krernertok/gophercises/cyoa/"
+)
+
 func main() {
-	defaultPath := "/mnt/c/dev/goworkspace/src/github.com/krernertok/gophercises/cyoa/data/gopher.json"
-	path := flag.String("file", defaultPath, "path for the story JSON file")
+	path := flag.String("file", defaultJSONPath, "path for the story JSON file")
 	firstArc := flag.String("first-arc", "intro", "name of the first story arc")
 	flag.Parse()
 
@@ -22,7 +27,6 @@ func main() {
 		return
 	}
 
-	templatePath := "/mnt/c/dev/goworkspace/src/github.com/krernertok/gophercises/cyoa/templates/arc.html"
 	templateName := filepath.Base(templatePath)
 	template, err := template.New(templateName).ParseFiles(templatePath)
 	if err != nil {
@@ -51,17 +55,14 @@ func (h storyHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	var found bool
 
 	path := r.URL.Path
-	// this should come from somewhere else, a configuration file?
-	// or should serving CSS files be done somehow differently?
-	baseDir := "/mnt/c/dev/goworkspace/src/github.com/krernertok/gophercises/cyoa/"
 
 	switch {
-	case path == "/":
-		arc, found = h.story[h.firstArc]
 	case strings.HasPrefix(path, "/css/"):
 		cssPath := baseDir + strings.TrimLeft(path, "/")
 		http.ServeFile(w, r, cssPath)
 		return
+	case path == "/":
+		arc, found = h.story[h.firstArc]
 	default:
 		arc, found = h.story[path[1:]]
 	}
@@ -74,6 +75,6 @@ func (h storyHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	err := h.template.Execute(w, arc)
 
 	if err != nil {
-		panic(err)
+		http.Error(w, "Something went wrong.", http.StatusInternalServerError)
 	}
 }
