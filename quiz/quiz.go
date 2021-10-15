dce48cc512b3ae427538c068e509095f42ec6fa1
Antti Sivula <antti.j.sivula@gmail.com>
2021-10-15 23:43:46 +0300

    Add Quiz - Part 2

--- quiz/quiz.go
+++ quiz/quiz.go
@@ -3,41 +3,62 @@ package main
 import (
 	"bufio"
 	"encoding/csv"
+	"flag"
 	"fmt"
 	"os"
 	"strings"
+	"time"
 )
 
 func main() {
-	problemFile, err := os.Open("problems.csv")
+	file := flag.String("file", "problems.csv", "file with problems")
+	limit := flag.Int("limit", 30, "time limit in seconds (default 30)")
+	flag.Parse()
+
+	problemFile, err := os.Open(*file)
 	if err != nil {
-		panic(err)
+		fmt.Println("Issue opening the problems file:", *file)
+		os.Exit(1)
 	}
 	defer problemFile.Close()
 
 	csvReader := csv.NewReader(problemFile)
 	problems, err := csvReader.ReadAll()
 	if err != nil {
-		panic(err)
+		fmt.Println("Could not parse the CSV file:", file)
+		os.Exit(1)
 	}
 
 	numProblems := len(problems)
 	correctAnswers := 0
 	scanner := bufio.NewScanner(os.Stdin)
+	timer := time.NewTimer(time.Duration(*limit) * time.Second)
+	answers := make(chan string)
 
+Loop:
 	for i, problem := range problems {
 		prob := problem[0]
 		answer := problem[1]
 
 		fmt.Printf("Problem #%d: %s = ", i+1, prob)
-		scanner.Scan()
 
-		if err := scanner.Err(); err != nil {
-			panic(err)
-		}
-		input := strings.TrimSpace(strings.ToLower(scanner.Text()))
-		if input == answer {
-			correctAnswers++
+		go func() {
+			scanner.Scan()
+
+			if err := scanner.Err(); err != nil {
+				panic(err)
+			}
+			answers <- strings.TrimSpace(strings.ToLower(scanner.Text()))
+		}()
+
+		select {
+		case <-timer.C:
+			fmt.Println("\nYou ran out of time!")
+			break Loop
+		case input := <-answers:
+			if input == answer {
+				correctAnswers++
+			}
 		}
 	}
 
